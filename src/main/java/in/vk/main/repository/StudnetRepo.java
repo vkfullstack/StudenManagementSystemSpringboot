83d55834cfd94ee92c73ad29b61b2c7457dffc0f
vkfullstack <vijaykhetre36@gmail.com>
2026-05-01 21:02:50 +0530

    Implemented UserRepository interface extending JpaRepository for database access

--- src/main/java/in/vk/main/repository/StudnetRepo.java
+++ src/main/java/in/vk/main/repository/StudnetRepo.java
@@ -1,5 +1,9 @@
 package in.vk.main.repository;
 
-public interface StudnetRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import in.vk.main.Entity.Student;
+
+public interface StudnetRepo extends JpaRepository<Student, Long> {
 
 }
