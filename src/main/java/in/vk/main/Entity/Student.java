fa8821c59cce65ee919a71f1629d61cb3dbde133
vkfullstack <vijaykhetre36@gmail.com>
2026-05-01 20:51:41 +0530

    Implemented User entity with fields, getters/setters and JPA mappings for authentication system

--- src/main/java/in/vk/main/Entity/Student.java
+++ src/main/java/in/vk/main/Entity/Student.java
@@ -1,5 +1,48 @@
 package in.vk.main.Entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
 public class Student {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column
+	private long id;
+	@Column
+	private  String firstname;
+	@Column
+	private String lastname;
+	@Column
+	private String email;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
 }
